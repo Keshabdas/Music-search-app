f67afff736d2e17f272f5071cb16f5bae7e51651
keshabDas <keshab.das@vivant.me>
2020-09-03 11:44:55 +0530

    Updates

---
+++ precache-manifest.168684d0b6974bd4c1affa64b8de68da.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd709b407ccc1b336631f20049461974",
+    "url": "/Music-search-app/index.html"
+  },
+  {
+    "revision": "e6ac36b7120d5673941a",
+    "url": "/Music-search-app/static/css/2.f47422e6.chunk.css"
+  },
+  {
+    "revision": "d1f9bb59808f826b114c",
+    "url": "/Music-search-app/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "e6ac36b7120d5673941a",
+    "url": "/Music-search-app/static/js/2.e1a6222f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Music-search-app/static/js/2.e1a6222f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1f9bb59808f826b114c",
+    "url": "/Music-search-app/static/js/main.29efec8f.chunk.js"
+  },
+  {
+    "revision": "a2f8b5b971d7122b38e9",
+    "url": "/Music-search-app/static/js/runtime-main.41779252.js"
+  }
+]);
